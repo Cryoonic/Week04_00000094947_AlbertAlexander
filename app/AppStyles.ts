b1c6c81c2426c059917807709c4f646a70111f95
Cryoonic <159890721+Cryoonic@users.noreply.github.com>
2026-03-03 14:08:24 +0700

    Week04

---
+++ app/AppStyles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: "#f4f6f8",
+  },
+  card: {
+    marginBottom: 15,
+    borderRadius: 15,
+  },
+  cardContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+  },
+  textContainer: {
+    flex: 1,
+  },
+  button: {
+    marginTop: 8,
+    borderRadius: 10,
+  },
+});
+
+export default styles;
